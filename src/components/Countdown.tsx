6ea94e11d2c5c09a2f7ffbcd7ae343deb3c9c300
PGabriel20 <bielporreca@gmail.com>
2021-03-11 19:30:25 -0300

    Feat: app components

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,17 +1,58 @@
+import { useContext, useEffect, useState } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/Countdown.module.css';
 
+
+
 export function Countdown(){
+  const { minutes,
+      seconds,
+      hasFinished,
+      isActive,
+      startCountdown,
+      resetCountdown
+    } = useContext(CountdownContext);
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+  
+
   return(
-    <div className={styles.countdownContainer}>
-      <div>
-        <span>2</span>
-        <span>5</span>
+    <div>
+      <div className={styles.countdownContainer}>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
       </div>
-      <span>:</span>
-      <div>
-        <span>0</span>
-        <span>0</span>
-      </div>
+
+      { hasFinished ? (
+        <button disabled className={`${styles.countdownButton}`}>
+        {/* {isActive ? 'Abandonar ciclo' : 'Iniciar um ciclo'} */}
+          Ciclo encerrado
+        </button>
+      ): (
+        <>
+          { isActive ? (
+            <button type="button" onClick={resetCountdown} className={`${styles.countdownButton} ${styles.countdownButtonActive}`}>
+            {/* {isActive ? 'Abandonar ciclo' : 'Iniciar um ciclo'} */}
+              Abandonar ciclo
+            </button>
+          ):
+          (
+            <button type="button" onClick={startCountdown} className={styles.countdownButton}>
+              Iniciar ciclo
+            </button>
+          )}
+        </>
+      )}      
     </div>
   );
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,12 @@
 import { Profiler } from "inspector";
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile(){
+  const { level } = useContext(ChallengesContext);
+
   return(
     <div className={styles.profileContainer}>
       <img src="https://i.pinimg.com/originals/db/ef/a3/dbefa3fd491990cb78e7612cd29d9c61.jpg" alt="Eu msm"/>
@@ -10,7 +14,7 @@ export function Profile(){
         <strong>Paulo Gabriel</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
